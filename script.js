2176b7fe6fd3742a6ab47736e9633237ca8954d8
sanzuru <dasrein@gmail.com>
2019-06-28 15:04:52 -0400

    e

--- script.js
+++ script.js
@@ -19,26 +19,6 @@ function loadSites() {
     }
 }
 
-function startTime() {
-    var today=new Date();
-    var h=today.getHours();
-    var m=today.getMinutes();
-    var s=today.getSeconds();
-    h = checkTime(h);
-    m = checkTime(m);
-    s = checkTime(s);
-    document.getElementById('clock').innerHTML = h+":"+m+":"+s;
-    var t = setTimeout(function(){startTime()},500);
-}
-
-function checkTime(i) {
-    if (i<10) {i = "0" + i};  // add zero in front of numbers < 10
-    return i;
-}
-
-// initialize search autocompletion...
-$("#search").googleSuggest({secure: true});
-
 var request = new XMLHttpRequest();
 request.open('GET', 'https://api.cryptonator.com/api/ticker/link-usd');
 request.responseType = 'json';
